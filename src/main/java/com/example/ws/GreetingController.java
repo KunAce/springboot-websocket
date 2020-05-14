2e7c3b45c1ef586222ded2ebaf686b164075ac52
KunAce <wxkace@gmail.com>
2020-05-13 23:30:03 -0700

    Added the feature for point-to-point chatting using websocket

--- src/main/java/com/example/ws/GreetingController.java
+++ src/main/java/com/example/ws/GreetingController.java
@@ -1,15 +1,49 @@
 package com.example.ws;
 
-import org.springframework.messaging.Message;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+
+import java.security.Principal;
+
+
+//@Controller
+//public class GreetingController {
+//    @MessageMapping("/hello")
+//    @SendTo("/topic/greetings")
+//    public Message greeting(Message message) throws Exception {
+//        return message;
+//    }
+//}
+
+//@Controller
+//public class GreetingController {
+//    @Autowired
+//    SimpMessagingTemplate messagingTemplate;
+//    @MessageMapping("/hello")
+//    public void greeting(Message message) throws Exception {
+//        messagingTemplate.convertAndSend("/topic/greetings",message);
+//    }
+//}
 
 @Controller
 public class GreetingController {
+    @Autowired
+    SimpMessagingTemplate messagingTemplate;
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
     public Message greeting(Message message) throws Exception {
         return message;
     }
+    @MessageMapping("/chat")
+    public void chat(Principal principal, Chat chat) {
+        String from = principal.getName();
+        chat.setFrom(from);
+        messagingTemplate.convertAndSendToUser(chat.getTo(),
+                "/queue/chat",chat);
+    }
 }
+
+
